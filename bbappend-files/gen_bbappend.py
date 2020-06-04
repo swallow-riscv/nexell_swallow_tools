5103abc9a1eb8ac8cf663195cc31cdf808ba561b
Hyunseok Jung <hsjung@nexell.co.kr>
2020-06-04 13:38:24 +0900

    tools: gen_bbappend: fixed up bbappend script for nx-camera-enc.
    This patch fixes up bbapend script for nx-camera-enc.
    
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>
    Change-Id: Ic435c09fe0c90c82c65f9a8db74795f8720309bc
    Reviewed-on: https://review.gerrithub.io/c/swallow-riscv/nexell_swallow_tools/+/494884
    Reviewed-by: SeongO Park <ray@nexell.co.kr>
    Tested-by: SeongO Park <ray@nexell.co.kr>

--- bbappend-files/gen_bbappend.py
+++ bbappend-files/gen_bbappend.py
@@ -12,6 +12,7 @@ INDEX_PATCH=2
 R_NX_LIBS='/recipes-nexell-libs/nx-libs'
 R_TESTSUITE='/recipes-application/testsuite'
 R_SIMPLE_ENC_TEST='/recipes-application/simple-enc-test'
+R_NX_CAMERA_ENC='/recipes-application/nx-camera-enc'
 
 TEMPLATE1=[
     "### Nexell - For Yocto build with using local source, Below lines are auto generated codes",
@@ -50,7 +51,7 @@ HASH_RECIPENAME_PATH = {
 
     'testsuite_%.bbappend' :            ['ON',['/apps/testsuite',R_TESTSUITE,'/testsuite'],[]],
     'simple-enc-test_%.bbappend' :      ['ON',['/apps/simple-enc-test',R_SIMPLE_ENC_TEST,'/simple-enc-test'],[]],
-    'nx-camera-enc_%.bbappend' :        ['ON',['/apps/nx-camera-enct',R_SIMPLE_ENC_TEST,'/nx-camera-enc'],[]],
+    'nx-camera-enc_%.bbappend' :        ['ON',['/apps/nx-camera-enc',R_NX_CAMERA_ENC,'/nx-camera-enc'],[]],
 }
 
 def gen_bbappend_paths(curWorkingPath) :
